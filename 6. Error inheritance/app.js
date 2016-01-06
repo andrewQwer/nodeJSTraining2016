fb235b86b8f7b091132ff361c25ff425289dbb3b
akaravaychik <akaravaychik@servicechannel.com>
2016-01-06 10:36:22 +0300

    error inheritance

---
+++ 6. Error inheritance/app.js
@@ -0,0 +1,47 @@
+var util = require('util');
+
+function PhraseError(message) {
+    this.message = message;
+    Error.captureStackTrace(this);
+}
+util.inherits(PhraseError, Error);
+PhraseError.prototype.name = "PhraseError";
+
+
+function HttpError(status,message) {
+    this.message = message;
+    this.status = status;
+    Error.captureStackTrace(this);
+}
+util.inherits(HttpError, Error);
+HttpError.prototype.name = "HttpError";
+
+var phrases = {
+    "Hello": "Привет",
+    "World": "мир"
+}
+
+function getPhrase(name) {
+    if (!phrases[name]) {
+        throw new PhraseError("Нет такой фразы: " + name);
+    }
+    return phrases[name];
+}
+
+function makePage(url) {
+    if (url != 'index.html') {
+        throw new HttpError(404, "нет такой страницы");
+    }
+    return util.format("%s, %s!", getPhrase("Helldo"), getPhrase("World"));
+}
+try {
+    var page = makePage('index.html');
+    console.log(page);
+}catch(e){
+    if (e instanceof HttpError){
+        console.log(e.status, e.message);
+    } else{
+        console.error("Ошибка %s\n Сообщение: %s\n стек: %s", e.name, e.message, e.stack)
+    }
+}
+
