3a865ae428dc4a94214c4962cdda79b1b6a798bc
akaravaychik <akaravaychik@servicechannel.com>
2016-01-13 17:04:25 +0300

    mongoose added

--- 16. Express app/createDb.js
+++ 16. Express app/createDb.js
@@ -1,30 +1,44 @@
-var MongoClient = require('mongodb').MongoClient
-    , assert = require('assert');
+var mongoose = require('./libs/mongoose');
+var async = require('async');
 
-// Connection URL
-var url = 'mongodb://localhost:27017/ExpressApp';
-// Use connect method to connect to the Server
-MongoClient.connect(url, function(err, db) {
-    assert.equal(null, err);
-    console.log("Connected correctly to server");
-    insertDocuments(db, function(){
-        db.close();
-    })
+async.series([
+    open,
+    dropDatabase,
+    requireModels,
+    createUsers
+], function (err) {
+    console.log(arguments);
+    mongoose.disconnect();
+    process.exit(err ? 255 : 0);
 });
-var insertDocuments = function(db, callback) {
-    // Get the documents collection
-    var collection = db.collection('documents');
-    collection.remove({},function(err,data){
-        collection.insertMany([
-            {a : 1}, {a : 2}, {a : 3}
-        ], function(err, result) {
-            assert.equal(err, null);
-            assert.equal(3, result.result.n);
-            assert.equal(3, result.ops.length);
-            console.log("Inserted 3 documents into the document collection");
-            callback(result);
-        });
-    })
-    // Insert some documents
 
+function open(callback){
+    mongoose.connection.on('open', callback);
+}
+
+function dropDatabase(callback){
+    var db = mongoose.connection.db;
+    db.dropDatabase(callback);
+}
+
+function requireModels(callback){
+    require('./models/user');
+
+    async.each(Object.keys(mongoose.models), function(modelName, callback){
+        mongoose.models[modelName].ensureIndexes(callback);
+    }, callback);
+}
+
+function createUsers(callback){
+
+    var users = [
+        {username: 'Вася', password: 'supervasya'},
+        {username: 'Петя', password: '123'},
+        {username: 'admin', password: 'thetruehero'}
+    ];
+
+    async.each(users, function(userData, callback) {
+        var user = new mongoose.models.User(userData);
+        user.save(callback);
+    }, callback);
 }
